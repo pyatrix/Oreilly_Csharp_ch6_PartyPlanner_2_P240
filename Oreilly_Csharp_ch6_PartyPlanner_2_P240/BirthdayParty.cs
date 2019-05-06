d8a174cda35fa4ce39de852b2dd06a6da8db29c5
Peng <Peng@PENG-PC>
2019-05-06 10:41:50 +0800

    P241 finish

---
+++ Oreilly_Csharp_ch6_PartyPlanner_2_P240/BirthdayParty.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oreilly_Csharp_ch6_PartyPlanner_2_P240
+{
+    class BirthdayParty
+    {
+        public const int CostOfFoodPerPerson = 25;
+        public int NumberOfPeople { get; set; }
+        public bool FancyDecorations { get; set; }
+        public string CakeWriting { get;set; }
+        
+        public BirthdayParty(int numberOfPeople,bool fancyDecorations, string cakeWriting)
+        {
+            NumberOfPeople = numberOfPeople;
+            FancyDecorations = fancyDecorations;
+            CakeWriting = cakeWriting;
+        }
+
+        private int CakeSize()
+        {
+            if (NumberOfPeople <= 4)
+                return 8;
+            else
+                return 16;
+        }
+
+        private int MaxWritingLength()
+        {
+            if (CakeSize() == 8)
+                return 16;
+            else
+                return 40;
+        }
+
+        private int ActualLength
+        {
+            get
+            {
+                if (CakeWriting.Length > MaxWritingLength())
+                    return MaxWritingLength();                
+                else
+                    return CakeWriting.Length;                
+            }
+        }
+    }
+}
